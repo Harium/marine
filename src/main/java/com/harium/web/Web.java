8ff323c0dc3b096cf8880067d93a99096c81d1a8
Yuri Pourre <yuripourre@gmail.com>
2017-12-09 17:21:15 -0200

    Add port, host and enableCORS methods

--- src/main/java/com/harium/web/Web.java
+++ src/main/java/com/harium/web/Web.java
@@ -12,10 +12,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static spark.Spark.before;
+import static spark.Spark.options;
 import static spark.Spark.staticFiles;
 
 public class Web {
 
+    private static int port;
     private static String host = "";
     public static final String PARAM_PATH = "path";
 
@@ -45,7 +48,7 @@ public class Web {
      * Method to treat paths with end slash
      */
     public static void acceptEndSlash() {
-        Spark.before(new Filter() {
+        before(new Filter() {
             @Override
             public void handle(Request request, Response response) throws Exception {
                 String path = request.pathInfo();
@@ -84,5 +87,43 @@ public class Web {
 
     public static void host(String host) {
         Web.host = host;
+    }
+
+    public static void port(int port) {
+        Web.port = port;
+        Spark.port(port);
+    }
+
+    public static void enableCORS(final String origin, final String methods, final String headers) {
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            // Note: this may or may not be necessary in your particular application
+            response.type("application/json");
+        });
+    }
+
+    public static String host() {
+        return host;
+    }
+
+    public static int port() {
+        return port;
     }
 }
